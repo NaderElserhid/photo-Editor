ca8aa568f83d3784c43a43f97a63b6e3b4c841f7
nadir <nadirelserhid@gmail.com>
2025-04-27 22:32:11 +0300

    dasd

--- back-end/middlewares/verifyToken.js
+++ back-end/middlewares/verifyToken.js
@@ -1,1 +1,16 @@
 import jwt from "jsonwebtoken";
+
+
+
+
+export default function verifyToken(req, res, next) {       
+    const token = req.cookies.access_token;
+    if (!token) return res.status(401).json("You are not authenticated!");
+    
+    jwt.verify(token, process.env.JWT, (err, user) => {
+        if (err) return res.status(403).json("Token is not valid!");
+        req.user = user;
+        next();
+    });
+    }   
+        
